fd8dc281fa95481d8cb653a2def604a81c430fe0
xiangxw <xiangxw5689@126.com>
2014-10-22 15:31:04 +0800

    add icon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
     MainWindow win;
 
+    app.setWindowIcon(QIcon(":images/saliency-marker.svg"));
     win.show();
     return app.exec();
 }
